592e2ae8e5e794f51b62778b9ad7f5bb7cedad06
jiyeon <journiyoon@gmail.com>
2025-07-21 20:14:09 +0900

    Fix prettier of baseurl line

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'https://mini-was-f5drgndedec9cxh6.koreacentral-01.azurewebsites.net/',
+	baseURL:
+		'https://mini-was-f5drgndedec9cxh6.koreacentral-01.azurewebsites.net/',
 })
 
 instance.defaults.headers.post['Content-Type'] =
